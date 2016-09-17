4b9a1543baa8433a1ec8b461e73c46580c40ae65
Harlan Barker <barker.harlan.r@student.uta.fi>
2016-09-17 18:50:08 +0300

    beta runner

---
+++ tfbs_footprinter-runner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+ 
+"""Convenience wrapper for running tfbs_footprinter directly from source tree."""
+ 
+ 
+from tfbs_footprinter.tfbs_footprinter import main
+ 
+ 
+if __name__ == '__main__':
+    main()
